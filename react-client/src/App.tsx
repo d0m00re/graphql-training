ed5366476cb894927e510fe4026d8bd1265d1275
d0m00re <helsonalexis@gmx.com>
2024-11-14 10:04:39 +0100

    (+) initial commit

---
+++ react-client/src/App.tsx
@@ -0,0 +1,12 @@
+// src/App.tsx
+import React from "react"
+
+const App: React.FC = () => {
+  return (
+    <div className="bg-zinc-800 flex-col h-screen w-full flex items-center justify-center p-4 gap-y-12 overflow-scroll">
+      <h2 className="text-4xl text-yellow-500">Hello World!</h2>
+    </div>
+  )
+}
+
+export default App
